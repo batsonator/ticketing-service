20413b1c0e3363f9442e7b81b407821b2d892ee9
Dan Batson <batsond@outlook.com>
2021-03-17 16:45:22 +0000

    first commit

---
+++ src/main/java/com/batsonator/ticketing/TicketingApp.java
@@ -0,0 +1,13 @@
+package com.batsonator.ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingApp {
+
+	public static void main( String[] args ) {
+
+		SpringApplication.run( TicketingApp.class, args );
+	}
+}
